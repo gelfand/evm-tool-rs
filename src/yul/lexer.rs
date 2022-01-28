1b79bb8f0e354025bad63722ce7a7b5138435f5a
eugene <eugene@linux.com>
2022-01-28 12:30:20 +0200

    lexer: block comments and idents

--- src/yul/lexer.rs
+++ src/yul/lexer.rs
@@ -4,6 +4,12 @@ pub struct Token {
     pub len: usize,
 }
 
+impl Token {
+    pub fn new(kind: TokenKind, len: usize) -> Self {
+        Self { kind, len }
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 pub enum TokenKind {
     /// "// comment"
@@ -45,44 +51,148 @@ pub enum Base {
 }
 
 pub fn parse(input: &'static str) -> Vec<Token> {
-    let mut tokens = Vec::new();
+    let mut tokens: Vec<Token> = Vec::new();
     let mut chars = input.chars();
     let mut _pos = 0;
     let mut _line = 1;
 
     while let Some(c) = chars.next() {
-        let kind: TokenKind = match c {
+        let token: Token = match c {
             '/' => {
                 if let Some(c) = chars.next() {
                     match c {
-                        '/' => TokenKind::LineComment,
-                        '*' => TokenKind::BlockComment,
-                        _ => TokenKind::InvalidIdentifier,
+                        '/' => {
+                            let mut comment = String::new();
+                            while let Some(c) = chars.next() {
+                                if c == '\n' {
+                                    break;
+                                }
+                                comment.push(c);
+                            }
+                            Token::new(TokenKind::LineComment, comment.len() + 2)
+                        }
+                        '*' => {
+                            let mut comment = String::new();
+                            while let Some(c) = chars.next() {
+                                if c == '*' {
+                                    if let Some(c) = chars.next() {
+                                        if c == '/' {
+                                            break;
+                                        }
+                                    }
+                                }
+                                comment.push(c);
+                            }
+                            Token::new(TokenKind::BlockComment, comment.len() + 1)
+                        }
+                        _ => Token::new(TokenKind::InvalidIdentifier, 1),
                     }
                 } else {
-                    TokenKind::InvalidIdentifier
+                    Token::new(TokenKind::InvalidIdentifier, 1)
                 }
             }
-            _ => {
-                todo!()
-            }
-        };
-
-        let len = match kind {
-            TokenKind::LineComment => {
-                let mut len = 0;
+            ' ' | '\t' | '\r' => {
+                let mut whitespace = String::new();
                 while let Some(c) = chars.next() {
-                    if c == '\n' {
+                    if c != ' ' && c != '\t' && c != '\r' {
                         break;
                     }
-                    len += 1;
+                    whitespace.push(c);
                 }
-                len + 2
+                Token::new(TokenKind::Whitespace, whitespace.len() + 1)
             }
-            _ => todo!(),
-        };
+            '\n' => {
+                _line += 1;
+                Token::new(TokenKind::Newline, 1)
+            }
+            ';' => Token::new(TokenKind::Semi, 1),
+            ',' => Token::new(TokenKind::Comma, 1),
+            '.' => Token::new(TokenKind::Dot, 1),
+            '(' => Token::new(TokenKind::OpenParenthesis, 1),
+            ')' => Token::new(TokenKind::CloseParenthesis, 1),
+            '{' => Token::new(TokenKind::OpenBrace, 1),
+            '}' => Token::new(TokenKind::CloseBrace, 1),
+            '[' => Token::new(TokenKind::OpenBracket, 1),
+            ']' => Token::new(TokenKind::CloseBracket, 1),
+            '"' => {
+                let mut value = String::new();
+                while let Some(c) = chars.next() {
+                    if c == '"' {
+                        break;
+                    }
+                    value.push(c);
+                }
 
-        tokens.push(Token { kind, len });
+                Token::new(
+                    TokenKind::Literal {
+                        kind: LiteralKind::String,
+                        value: value.clone(),
+                    },
+                    value.len(),
+                )
+            }
+            '0'..='9' => {
+                let mut value = String::new();
+                value.push(c);
+                let mut base_specified = false;
+                while let Some(c) = chars.next() {
+                    if !c.is_numeric() && c != 'x' && !base_specified {
+                        break;
+                    }
+                    if c == 'x' {
+                        base_specified = true;
+                    }
+                    value.push(c);
+                }
+                Token::new(
+                    TokenKind::Literal {
+                        kind: LiteralKind::Int {
+                            base: if base_specified {
+                                Base::Hexadecimal
+                            } else {
+                                Base::Decimal
+                            },
+                        },
+                        value: value.clone(),
+                    },
+                    value.len(),
+                )
+            }
+            _ => {
+                if c.is_alphabetic() {
+                    let mut value = String::new();
+                    while let Some(c) = chars.next() {
+                        if c.is_alphanumeric() {
+                            value.push(c);
+                        } else {
+                            break;
+                        }
+                    }
+                    Token::new(TokenKind::Identifier, value.len())
+                } else if c.is_numeric() {
+                    let mut value = String::new();
+                    while let Some(c) = chars.next() {
+                        if c.is_numeric() {
+                            value.push(c);
+                        } else {
+                            break;
+                        }
+                    }
+                    Token::new(
+                        TokenKind::Literal {
+                            kind: LiteralKind::Int {
+                                base: Base::Decimal,
+                            },
+                            value: value.clone(),
+                        },
+                        value.clone().len(),
+                    )
+                } else {
+                    Token::new(TokenKind::InvalidIdentifier, 1)
+                }
+            }
+        };
+        tokens.push(token);
     }
     tokens
 }
@@ -99,6 +209,41 @@ mod tests {
                 kind: TokenKind::LineComment,
                 len: 10,
             }]
+        );
+
+        assert_eq!(
+            parse("/* comment */"),
+            vec![Token {
+                kind: TokenKind::BlockComment,
+                len: 10,
+            }]
+        );
+    }
+
+    #[test]
+    fn parse_whitespace() {
+        assert_eq!(
+            parse(" \t\r\n"),
+            vec![Token {
+                kind: TokenKind::Whitespace,
+                len: 3,
+            }]
+        );
+    }
+
+    #[test]
+    fn parse_identifier() {
+        assert_eq!(
+            parse("0xffff"),
+            vec![Token::new(
+                TokenKind::Literal {
+                    kind: LiteralKind::Int {
+                        base: Base::Hexadecimal
+                    },
+                    value: "0xffff".to_string(),
+                },
+                6
+            )]
         );
     }
 }
